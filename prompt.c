9edc26fb6aa72f2e8883408070adfbaab0ab3ca0
Dinoab <delildin4@gmail.com>
2022-08-16 09:14:41 -0700

    simple shell command prompt

--- prompt.c
+++ prompt.c
@@ -1,144 +1,53 @@
 #include <stdio.h>
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
+#include <string.h>
+#include <stdlib.h>
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
- *
+ * realloc - Reallocates a memory block using malloc and free.
  * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int main(void)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+char *buffer = NULL, *copy_buffer = NULL;
+ssize_t num_read
+size_t n = 0;
+char *token;
+char *delim = " \n";
+int num_tokens = 0, i = 0;
+char **argv;
 
-	if (new_size == old_size)
-		return (ptr);
 
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-		if (mem == NULL)
-			return (NULL);
 
-		return (mem);
-	}
-
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-
-	free(ptr);
-	return (mem);
+printf(" $");
+getline(&buffer, &n, stdin);
+printf(">>>>> %s\n", buffer);
+/**************************/
+/* to copy the user typed allocating enogh space for it */
+copy_buffer = malloc(sizeof(char) * num_read);
+/* copy the contents of buffer to copy_buffer */
+strcpy(copy_buffer, buffer)
+/* get the number of tokens to be created */
+token = strtok(buffer, delim);
+while (token != NULL)
+{
+	num_tokens++;
+	token = strtok(NULL, delim);
 }
+num_tokens++;
 
-/**
- * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
- * @n: The size of lineptr.
- * @buffer: The string to assign to lineptr.
- * @b: The size of buffer.
- */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+argv = malloc(sizeof(char *) * num_tokens)
+/* get the tokens and save them as arguments in argv */
+token = strtok(copy_buffer, delim);
+	while (token != NULL)
 {
-	if (*lineptr == NULL)
-	{
-		if (b > 120)
-			*n = b;
-		else
-			*n = 120;
-		*lineptr = buffer;
+		/* allocate space for each token */
+		argv[i] = malloc(sizeof(char) * strlen(token));
+		strcpy(argv[i], token);
+		/* for debbuging purpose */
+		printf(">>>>> %s\n", argv[i]);
+		i++;
+		  token = strtok(NULL, delim);
 	}
-	else if (*n < b)
-	{
-		if (b > 120)
-			*n = b;
-		else
-			*n = 120;
-		*lineptr = buffer;
-	}
-	else
-	{
-		_strcpy(*lineptr, buffer);
-		free(buffer);
-	}
-}
-
-/**
- * _getline - Reads input from a stream.
- * @lineptr: A buffer to store the input.
- * @n: The size of lineptr.
- * @stream: The stream to read from.
- *
- * Return: The number of bytes read.
- */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
-{
-	static ssize_t input;
-	ssize_t ret;
-	char c = 'x', *buffer;
-	int r;
-
-	if (input == 0)
-		fflush(stream);
-	else
-		return (-1);
-	input = 0;
-
-	buffer = malloc(sizeof(char) * 120);
-	if (!buffer)
-		return (-1);
-
-	while (c != '\n')
-	{
-		r = read(STDIN_FILENO, &c, 1);
-		if (r == -1 || (r == 0 && input == 0))
-		{
-			free(buffer);
-			return (-1);
-		}
-		if (r == 0 && input != 0)
-		{
-			input++;
-			break;
-		}
-
-		if (input >= 120)
-			buffer = _realloc(buffer, input, input + 1);
-
-		buffer[input] = c;
-		input++;
-	}
-	buffer[input] = '\0';
-
-	assign_lineptr(lineptr, n, buffer, input);
-
-	ret = input;
-	if (r != 0)
-		input = 0;
-	printf("$ ");
-	return (ret);
+argv[i] = NULL;
+printf(">>>>> %s\n", buffer);
+/***************************/
 }
